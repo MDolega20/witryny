6cb6b311385a0b5375ea32728c70bb1eaad7c3e7
MDolega20 <mateusz.dolega.20@gmail.com>
2019-03-20 15:58:19 +0100

    quiz added

---
+++ 17-03-2018_quiz/main.js
@@ -0,0 +1,69 @@
+let json = '{ "response_code": 0, "results": [{ "category": "Entertainment: Music", "type": "multiple", "difficulty": "easy", "question": "Which member of the Foo Fighters was previously the drummer for Nirvana?", "correct_answer": "Dave Grohl", "incorrect_answers": ["Taylor Hawkins", "Nate Mendel", "Chris Shiflett"] }, { "category": "Entertainment: Film", "type": "multiple", "difficulty": "easy", "question": "This movie contains the quote, &quot;Houston, we have a problem.&quot;", "correct_answer": "Apollo 13", "incorrect_answers": ["The Right Stuff", "Capricorn One", "Marooned"] }, { "category": "Geography", "type": "multiple", "difficulty": "hard", "question": "The mountainous Khyber Pass connects which of the two following countries?", "correct_answer": "Afghanistan and Pakistan", "incorrect_answers": ["India and Nepal", "Pakistan and India", "Tajikistan and Kyrgyzstan"] }, { "category": "Politics", "type": "multiple", "difficulty": "medium", "question": "The 2014 movie &quot;The Raid 2: Berandal&quot; was mainly filmed in which Asian country?", "correct_answer": "Indonesia", "incorrect_answers": ["Thailand", "Brunei", "Malaysia"] }, { "category": "Science & Nature", "type": "multiple", "difficulty": "medium", "question": "What does the yellow diamond on the NFPA 704 fire diamond represent?", "correct_answer": "Reactivity", "incorrect_answers": ["Health", "Flammability", "Radioactivity"] }, { "category": "Geography", "type": "multiple", "difficulty": "medium", "question": "The land mass of modern day Turkey is called what?", "correct_answer": "Anatolia", "incorrect_answers": ["Ismuth of Ottoma", "Ottoma", "Ismuth of Anatolia"] }, { "category": "Entertainment: Video Games", "type": "multiple", "difficulty": "hard", "question": "During development of &quot;Super Mario World&quot;, Yoshi&#039;s hard saddle was originally which of these?", "correct_answer": "A shell", "incorrect_answers": ["A slide of Gelatin", "A poffin", "A spike"] }, { "category": "Vehicles", "type": "multiple", "difficulty": "easy", "question": "What are the cylinder-like parts that pump up and down within the engine?", "correct_answer": "Pistons", "incorrect_answers": ["Leaf Springs", "Radiators", "ABS"] }, { "category": "Entertainment: Video Games", "type": "multiple", "difficulty": "hard", "question": "In Guild Wars 2, what is the name of the legendary Balthazar-themed torch?", "correct_answer": "Flames of War", "incorrect_answers": ["Sunrise", "Incinerator", "The Flameseeker Prophecies"] }, { "category": "History", "type": "multiple", "difficulty": "easy", "question": "Who led the Communist Revolution of Russia?", "correct_answer": "Vladimir Lenin", "incorrect_answers": ["Joseph Stalin", "Vladimir Putin", "Mikhail Gorbachev"] }] }';
+let json2 = '{"response_code":0,"results":[{"category":"History","type":"multiple","difficulty":"medium","question":"What event marked the start of World War II?","correct_answer":"Invasion of Poland (1939)","incorrect_answers":["Invasion of Russia (1942)","Battle of Britain (1940)","Invasion of Normandy (1944)"]},{"category":"Science & Nature","type":"multiple","difficulty":"medium","question":"What is the colour of unoxidized blood?","correct_answer":"Red","incorrect_answers":["Blue","Purple","Green"]},{"category":"Politics","type":"multiple","difficulty":"medium","question":"Who was the only president to not be in office in Washington D.C?","correct_answer":"George Washington","incorrect_answers":["Abraham Lincoln","Richard Nixon","Thomas Jefferson"]},{"category":"Entertainment: Video Games","type":"multiple","difficulty":"hard","question":"The map featured in Arma 3 named &quot;Altis&quot; is based off of what Greek island?","correct_answer":"Lemnos","incorrect_answers":["Ithaca","Naxos","Anafi"]},{"category":"Entertainment: Video Games","type":"multiple","difficulty":"medium","question":"What is the original name of Final Fantasy XV?","correct_answer":"Final Fantasy Versus XIII","incorrect_answers":["Final Fantasy: Reborn","Final Fantasy XVI","Final Fantasy XIII-3"]},{"category":"Entertainment: Video Games","type":"boolean","difficulty":"medium","question":"&quot;Metal Gear Solid 3: Snake Eater&quot; takes place on Shadow Moses Island.","correct_answer":"False","incorrect_answers":["True"]},{"category":"History","type":"boolean","difficulty":"easy","question":"The United States Department of Homeland Security was formed in response to the September 11th attacks.","correct_answer":"True","incorrect_answers":["False"]},{"category":"Entertainment: Japanese Anime & Manga","type":"multiple","difficulty":"easy","question":"What is the last name of Edward and Alphonse in the Fullmetal Alchemist series.","correct_answer":"Elric","incorrect_answers":["Ellis","Eliek","Elwood"]},{"category":"Geography","type":"multiple","difficulty":"hard","question":"What year is on the flag of the US state Wisconsin?","correct_answer":"1848","incorrect_answers":["1634","1783","1901"]},{"category":"Science & Nature","type":"multiple","difficulty":"medium","question":"What stage of development do the majority of eukaryotic cells remain in for most of their life?","correct_answer":"Interphase","incorrect_answers":["Prophase","Stasis","Telophase"]},{"category":"Animals","type":"multiple","difficulty":"easy","question":"What is Grumpy Cat&#039;s real name?","correct_answer":"Tardar Sauce","incorrect_answers":["Sauce","Minnie","Broccoli"]},{"category":"Science: Computers","type":"multiple","difficulty":"hard","question":"Who is the original author of the realtime physics engine called PhysX?","correct_answer":"NovodeX","incorrect_answers":["Ageia","Nvidia","AMD"]},{"category":"Entertainment: Video Games","type":"multiple","difficulty":"hard","question":"What was the name of the game created by Masahiro Sakurai and Satoru Iwata that would eventually evolve into &quot;Super Smash Bros.&quot;?","correct_answer":"Dragon King: The Fighting Game","incorrect_answers":["Nintendo Legends: Fight!","Martial Arts: King of the Hill","Aerial Warriors: Supreme Battle"]},{"category":"Entertainment: Film","type":"multiple","difficulty":"medium","question":"What is the correct spelling of the protagonist of the book in The NeverEnding Story (1984)?","correct_answer":"Atreyu","incorrect_answers":["Atrayu","Atraiyu","Atraeyu"]},{"category":"Entertainment: Film","type":"multiple","difficulty":"medium","question":"In which movie does Robin Williams&#039; character have to disguise themselves into a woman?","correct_answer":"Mrs. Doubtfire","incorrect_answers":["Old Dogs","Jumanji","Awakenings"]},{"category":"Entertainment: Cartoon & Animations","type":"multiple","difficulty":"easy","question":"What is Everest&#039;s favorite food in the Nickelodeon\/Nick Jr. series &quot;PAW Patrol&quot;?","correct_answer":"Liver","incorrect_answers":["Chicken","Steak","Caribou"]},{"category":"Entertainment: Video Games","type":"multiple","difficulty":"hard","question":"The &#039;D&#039; in the term &quot;D-Mail&quot; from the game &quot;Steins; Gate&quot; is short for what?","correct_answer":"DeLorean","incorrect_answers":["Deep","Demo","Dev"]},{"category":"Entertainment: Comics","type":"multiple","difficulty":"hard","question":"In the &quot;Archie&quot; comics, who was Jughead&#039;s first girlfriend?","correct_answer":"Joani","incorrect_answers":["Ethel","Debbi","Margret"]},{"category":"Politics","type":"multiple","difficulty":"medium","question":"In United States history, how many vice presidents did Franklin D. Roosevelt have during his time in office as president?","correct_answer":"3","incorrect_answers":["1","2","0"]},{"category":"Entertainment: Comics","type":"multiple","difficulty":"medium","question":"Which &quot;Green Arrow&quot; sidekick commonly wears a baseball cap?","correct_answer":"Roy Harper","incorrect_answers":["Black Canary","Emiko Queen","Dick Grayson"]}]}';
+
+let data = JSON.parse(json2);
+// console.log(data);
+
+let container = document.createElement("div");
+container.classList.add("quiz_container");
+
+data.results.forEach(element => {
+    console.log(element);
+    let item = document.createElement("div");
+    item.classList.add("item");
+    let question = document.createElement("h3");
+    let list = document.createElement("ul");
+    question.innerText = element.question;
+    let ansers = [];
+    element.incorrect_answers.forEach(element1 => {
+        let x = {
+            true: false,
+            content: element1
+        };
+        ansers.push(x);
+    });
+    let x = {
+        true: true,
+        content: element.correct_answer
+    }
+    ansers.push(x);
+    ansers.forEach(element2 => {
+        let ansuer = document.createElement("li");
+        let label = document.createElement("label");
+        let check = document.createElement("input");
+        check.type = "checkbox";
+
+        let ans = document.createElement("p");
+        ans.innerText = element2.content;
+
+        label.appendChild(check);
+        label.appendChild(ans);
+        if (element2.true === true) {
+            ansuer.classList.add("true");
+        } else {
+            ansuer.classList.add("false");
+        }
+        if(element2.true === true){
+            label.onclick = function () { selectAnsuer(1); };
+        }
+        ansuer.appendChild(label);
+        list.appendChild(ansuer);
+    });
+    item.appendChild(question);
+    item.appendChild(list);
+    container.appendChild(item);
+});
+
+document.getElementById("quiz").appendChild(container);
+console.log("Quiz loaded");
+let punkty = 0; //max 40
+function selectAnsuer(pt){
+    console.log("Punkt +");
+    punkty+= pt;
+    let wynik = document.createElement("h2");
+    wynik.innerText = (punkty + "/" + data.results.length * 2) ;
+    document.getElementById("score").innerHTML = "";
+    document.getElementById("score").appendChild(wynik);
+}
+
+// selectAnsuer();

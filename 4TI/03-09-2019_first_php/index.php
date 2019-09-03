9b013bc1be25b7b3109a51fd1ea3555c455f9c81
MDolega20 <mateusz.dolega.20@gmail.com>
2019-09-03 09:00:40 +0200

    4ti 1

---
+++ 4TI/03-09-2019_first_php/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Php</title>
+</head>
+<body>
+    <?php 
+        echo "Text generated at server side by php ";
+    ?>
+</body>
+</html>
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # witryny
+
+## Php linux server command
+php -S localhost:8000

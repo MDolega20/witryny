8b93bd9253178bcd3391bab54f1038c6cebb5444
MDolega20 <mateusz.dolega.20@gmail.com>
2019-09-17 14:26:11 +0200

    .

--- 4TI/17-09-2019_strings/zadanie_8.php
+++ 4TI/17-09-2019_strings/zadanie_8.php
@@ -1,1 +1,15 @@
 <?php
+
+$adress_s = "https://www.google.pl/#f49fbbbread;https://www.google.pl/bread;https://www.google.pl/bread;https://www.google.pl/bread";
+$adress = explode(";",$adress_s);
+$links = [];
+
+foreach ($adress as $key => $value) {
+    array_push($links, "<a href=\"".$value."\">".$value."</a>");
+}
+
+foreach ($links as $key => $value) {
+    echo $value . "\n";
+}
+
+echo "Liczba linków: " . count($links) . "\n";

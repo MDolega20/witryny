c033a7b3be9ca99f64697dcc3b41225238b2f63f
MDolega20 <mateusz.dolega.20@gmail.com>
2019-09-18 14:16:45 +0200

    .

---
+++ 4TI/18-09-2019_functions/zadanie.php
@@ -0,0 +1,104 @@
+<?php
+
+function roznica($a, $b)
+{
+    if (is_numeric($a) && is_numeric($b)) {
+        wyswietl($a, $b, ($a - $b), "-");
+    } else {
+        echo "error\n";
+    }
+}
+function sum($a, $b, $c)
+{
+    if (is_numeric($a) && is_numeric($b) && is_numeric($c)) {
+        wyswietl($a, $b, ($a + $b), "+");
+    } else {
+        echo "BŁĄD\n";
+    }
+}
+function iloczyn($a, $b)
+{
+    if (is_numeric($a) && is_numeric($b)) {
+        wyswietl($a, $b, ($a * $b), "*");
+    } else {
+        echo "error\n";
+    }
+}
+function iloraz($a, $b)
+{
+    if (is_numeric($a) && is_numeric($b) && $a != 0) {
+        wyswietl($a, $b, ($a / $b), "/");
+    } else {
+        echo "error\n";
+    }
+}
+function potega($a, $b)
+{
+    $result;
+    if (is_int($a) && is_int($b) && $b >= 0) {
+        if ($b == 1) {
+            $result = $a;
+        } else if ($b == 0) {
+            $result = 1;
+        } else {
+            $result = $a;
+            for ($i = 1; $i < $b; $i++) {
+                $result = $result * $result;
+            }
+
+        }
+    } else {
+        echo "error\n";
+    }
+
+    if ($result !== null) {
+        wyswietl($a, $b, $result, "^");
+    } else {
+        echo "error\n";
+    }
+}
+function wyswietl($l1, $l2, $w, $dz)
+{
+    echo $l1 . " " . $dz . " " . $l2 . " = " . $w . "\n";
+}
+function squere($n, $x)
+{
+    if ($n > 0 && strlen($x) === 1) {
+
+        for ($i = 1; $i <= $n; $i++) {
+            echo $x;
+        }
+        echo "\n";
+
+        for ($i = 1; $i <= $n - 2; $i++) {
+            for ($j = 1; $j <= $n; $j++) {
+                if ($j === 1 || $j === $n) {
+                    echo $x;
+                } else {
+                    echo " ";
+                }
+            }
+            echo "\n";
+        }
+
+        for ($i = 1; $i <= $n; $i++) {
+            echo $x;
+        }
+        echo "\n";
+
+        return 1;
+    } else {
+        echo "BŁAÐ\n";
+        return -1;
+    }
+}
+function sort_this($array){
+    print_r($array);
+    $x = sort($array);
+    print_r($x);
+    // return reverse(sort($array));
+}
+
+//test
+// squere(7, "O");
+sort_this([15,6,7,8,4444]);

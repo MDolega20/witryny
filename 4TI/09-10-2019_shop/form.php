0554ce0a598c672a2d85917fea14e5387eea9ac1
MDolega20 <mateusz.dolega.20@gmail.com>
2019-10-15 11:17:35 +0200

    .

--- 4TI/09-10-2019_shop/form.php
+++ 4TI/09-10-2019_shop/form.php
@@ -1,9 +1,24 @@
 <form method="post" id="form-price">
-    <label><span>od:</span><input type="number" name="min" value="<?php echo $_POST["min"]; ?>"></label>
-    <label><span>do:</span><input type="number" name="max" value="<?php echo $_POST["max"]; ?>"></label>
-    <input type="submit" value="Przefiltruj">
-</form>
-<form method="post" id="form-search">
-    <label><input type="text" name="product_name" value="<?php echo $_POST["product_name"]; ?>"></label>
+    <div>
+        <label><span>od:</span><input type="number" name="min" value="<?php echo $_POST["min"]; ?>"></label>
+        <label><span>do:</span><input type="number" name="max" value="<?php echo $_POST["max"]; ?>"></label>
+    </div>
+    <div>
+        <label><input type="text" name="product_name" value="<?php echo $_POST["product_name"]; ?>"></label>
+    </div>
+    <div>
+        <label>
+            <select name="sort_type">
+                <option value="nazwa" >nazwa</option>
+                <option value="cena">cena</option>
+            </select>
+        </label>
+        <label>
+            <select name="sort_esc">
+                <option value="ASC" >rosnąco</option> //todo
+                <option value="ESC">malejąco</option>
+            </select>
+        </label>
+    </div>
     <input type="submit" value="Szukaj">
 </form>
--- 4TI/09-10-2019_shop/tabela.php
+++ 4TI/09-10-2019_shop/tabela.php
@@ -8,6 +8,10 @@ if(isset($_POST["max"]) && !empty($_POST["max"]))
     $max = $_POST["max"];
 if(isset($_POST["product_name"]) && !empty($_POST["product_name"]))
     $product_name = $_POST["product_name"];
+if(isset($_POST["sort_type"]) && !empty($_POST["sort_type"]))
+    $sort_type = $_POST["sort_type"];
+if(isset($_POST["sort_esc"]) && !empty($_POST["sort_esc"]))
+    $sort_esc = $_POST["sort_esc"];
 
 $polaczenie = mysqli_connect('localhost', 'mateusz', '123', 'sklep_spożywczy');
 
@@ -24,10 +28,15 @@ if ($polaczenie) {
         if(isset($product_name)){
             $query = $query . (isset($min) || isset($max) ? " AND" : "") . " nazwa LIKE \"%" . $product_name . "%\"";
         }
-        // $query = $query . " ORDER BY cena";
+        if(isset($sort_type) && isset($sort_esc)){
+            $query = $query . " ORDER BY " . $sort_type . " " . $sort_esc;
+        }
     }
 
     $wynik = mysqli_query($polaczenie, $query);
+
+    echo $query;
+    // print_r($_POST);
 
     if ($wynik) {
         echo "<table>";

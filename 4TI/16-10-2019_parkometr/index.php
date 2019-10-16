3e713a00ca510630759ed94ba13aa30efcca221b
MDolega20 <mateusz.dolega.20@gmail.com>
2019-10-16 15:03:07 +0200

    .

---
+++ 4TI/16-10-2019_parkometr/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Parkometr</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div id="parkmetr">
+        <div>
+            <h1>Parkometr</h1>
+        <div id="cennik">
+            <div class="cennik-row-header">
+                <p>pon-pt.</p>
+                <p>9-17</p>
+            </div>
+            <div class="cennik-row">
+                <p>0.5h</p>
+                <p>1</p>
+            </div>
+            <div class="cennik-row">
+                <p>1h</p>
+                <p>2</p>
+            </div>
+            <div class="cennik-row">
+                <p>2h</p>
+                <p>4</p>
+            </div>
+            <div class="cennik-row">
+                <p>3h</p>
+                <p>6</p>
+            </div>
+            <div class="cennik-row">
+                <p>4h+ (za godzinę)</p>
+                <p>1.5</p>
+            </div>
+            <div class="cennik-row">
+                <p>24h</p>
+                <p>12</p>
+            </div>
+        </div>
+        </div>
+        <div>
+            <div id="money_input">
+            </div>
+            <div id="money_output">
+                <div class="money_output_item">
+                    5PLN
+                </div>
+            </div>
+            <div id="controls">
+
+            </div>
+            <div id="ticket">
+            </div>
+        </div>
+        
+    </div>
+</body>
+</html>
